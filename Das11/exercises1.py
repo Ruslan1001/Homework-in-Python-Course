2e2898c91a359e3e1841a285dd26d6aedba76dbb
Ruslan1001 <157042172+Ruslan1001@users.noreply.github.com>
2024-01-22 20:22:28 +0400

    Add files via upload

---
+++ Das11/exercises1.py
@@ -0,0 +1,7 @@
+# 1.Max of Three
+# Write a Python function to find the Max of three numbers.
+def max_of_three(*number):
+    for i in number:
+        if i<
+    return i 
+print(max_of_three(25,22,563,225,453)) 
